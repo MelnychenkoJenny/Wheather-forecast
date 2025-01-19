4fc4f92542d0f9651b1f516a221ce23221420a28
Jenny Melnychenko <melnichenkojenny@gmail.com>
2025-01-19 13:17:14 +0200

    Add styles colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+  accent: '#EB9A21',
+  text: '#4E4C4C',
+  transparent: 'transparent',
+  defaultLight: '#FFFFFF',
+  defaultDark: '#000000',
+};
---
+++ src/styles/types.ts
@@ -0,0 +1,3 @@
+import { colors } from './colors';
+
+export type ColorsType = keyof typeof colors;
