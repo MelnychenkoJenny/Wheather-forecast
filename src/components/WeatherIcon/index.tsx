451b071907e2b58afae4aab53c043e72d1308570
Jenny Melnychenko <melnichenkojenny@gmail.com>
2025-01-20 11:21:14 +0200

    Update styles

--- src/components/WeatherIcon/index.tsx
+++ src/components/WeatherIcon/index.tsx
@@ -2,16 +2,16 @@ import React, { FC } from 'react';
 import { Image } from 'react-native';
 
 import { allWeathers } from '@src/assets/weatherIcon';
-
 import { colors } from '@src/styles';
+
 import { WeatherIconPropsType } from './types';
 
 export const WeatherIcon: FC<WeatherIconPropsType> = ({ code }) => (
   <Image
     alt={'Weather icon'}
-    width={5}
+    width={40}
     resizeMode={'stretch'}
     source={code && allWeathers[code]}
-    style={{ aspectRatio: 1.1, width: 70, tintColor: colors.accent }}
+    style={{ aspectRatio: 1.1, width: 40, tintColor: colors.accent }}
   />
 );
